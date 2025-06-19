98b642b6062a1d92896bfbf1f8b79a8345e52697
Rafael Dias Ferreira <rafael.diasferreira@ticketmaster.com>
2025-06-19 16:42:34 -0300

    heapsort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -3,9 +3,8 @@ package sorting;
 //average: O(n^2), worst: O(n^2), best: O(n^2) since it always goes through the entire array
 public class SelectionSort {
     public static void selectionSort(int[] arr) {
-        long startTime = System.currentTimeMillis();
-
         int n = arr.length;
+        
         for (int i = 0; i < n - 1; i++) {
             // Find the minimum element in unsorted array
             int minIndex = i;
@@ -19,11 +18,6 @@ public class SelectionSort {
             arr[minIndex] = arr[i];
             arr[i] = temp;
         }
-
-        long endTime = System.currentTimeMillis();
-        System.out.println("Start time: " + startTime + " ms");
-        System.out.println("End time: " + endTime + " ms");
-        System.out.println("Duration: " + (endTime - startTime) + " ms");
     }
 
     public static void main(String[] args) {
